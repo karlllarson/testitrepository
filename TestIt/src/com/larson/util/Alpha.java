3ce8e3f10011cb925f543e651f9a18367162cce4
Karl Larson <kllarson@hotmail.com>
2021-05-16 10:36:48 -0500

    Committing new repository

---
+++ TestIt/src/com/larson/util/Alpha.java
@@ -0,0 +1,7 @@
+package com.larson.util;
+
+public class Alpha {
+	public void sayHi() {
+		System.out.println("Alpha says hi");
+	}
+}
---
+++ TestIt/src/com/larson/util/Main.java
@@ -0,0 +1,7 @@
+package com.larson.util;
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
